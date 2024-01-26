29e5b00003f37bf927cd223d0f93544f8bdaa0c0
bamansour1978 <karatman@gmail.com>
2024-01-26 22:52:40 +0100

    2. Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Module documentation
+"""
+import csv
+from typing import Tuple, List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+    
+    @staticmethod
+    def assert_positive_integer_type(value: int) -> None:
+        """
+        Function documentation
+        """
+        assert type(value) is int and value > 0
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Function documentation
+        """
+        self.assert_positive_integer_type(page)
+        self.assert_positive_integer_type(page_size)
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+        try:
+            data = dataset[start:end]
+        except IndexError:
+            data =[]
+        return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Function documentation
+        """
+        total_pages = len(self.dataset()) // page_size + 1
+        data = self.get_page(page, page_size)
+        info = {
+            "page": page,
+            "page_size": page_size if page_size <= len(data) else len(data),
+            "total_pages": total_pages,
+            "data": data,
+            "prev_page": page - 1 if page > 1 else None,
+            "next_page": page + 1 if page + 1 <= total_pages else None
+        }
+        return info

629ad0ee86f5e2682471a24d76fd0235820fa776
bamansour1978 <karatman@gmail.com>
2024-01-31 22:48:16 +0100

    chmod

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -23,7 +23,6 @@ class LIFOCache(BaseCaching):
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data.keys():
-                first_key = next(iter(self.cache_data.keys()))
                 last_key, last_value = self.cache_data.popitem()
                 print("DISCARD: {}". format(first_key))
 

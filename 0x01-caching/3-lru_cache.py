97360abb57df2daa9429f73a22c37e535d0949b8
bamansour1978 <karatman@gmail.com>
2024-01-31 23:10:44 +0100

    3. LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module documentation
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class documentation
+    """
+
+    def __init__(self):
+        """inti
+        """
+        super().__init__()
+        self.usedKeys = []
+
+    def put(self, key, item):
+        """Function documentation
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {:s}". format(discard))
+
+    def get(self, key):
+        """Function documentation
+        """
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None

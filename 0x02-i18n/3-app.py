00023771310ae982166a740e4b7006457818c6cc
bamansour1978 <karatman@gmail.com>
2024-02-06 23:09:43 +0100

    Up

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 """
-Module documentation
+A Basic flask application
 """
 from flask import Flask
+from flask import request
 from flask import render_template
 from flask_babel import Babel
-from flask import request
 
 
 class Config(object):
     """
-    class documentation
+    Application configuration class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Wrap the application with Babel
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """
-    Function documentation
+    Gets locale from request object
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -33,7 +36,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """
-    Function documentation
+    Renders a basic html template
     """
     return render_template('3-index.html')
 
